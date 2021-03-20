331f1be24017de781c667ea3a10e4b0a163438db
mambru <ecastro82@gmail.com>
2021-03-20 18:39:31 -0500

    login with password check included

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -94,4 +94,29 @@ router.delete('/:id', (req, res) => {
     })
 });
 
+router.post('/login', (req, res) => {
+
+    // Query operation
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if(!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address! '});
+            return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if(!validPassword) {
+            res.status(400).json({ message: 'Incorrect password! '});
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+
+        // Verify user
+    })
+
+})
+
 module.exports = router;
